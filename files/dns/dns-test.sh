552388fccf85fc63f65a6b6404679e618eff99af
Alvaro Garcia <alvarogarciamarquez.ccg@gmail.com>
2025-02-04 12:59:55 +0100

    feat: dns script moved on the dns folder

---
+++ files/dns/dns-test.sh
@@ -0,0 +1,15 @@
+set -euo pipefail
+
+function resolve () {
+    dig $nameserver +short $@
+}
+
+nameserver=@$1
+
+resolve ns.sri.ies
+resolve ftp.sri.ies
+resolve mirror.sri.ies
+
+resolve -x 192.168.57.10
+resolve -x 192.168.57.20
+resolve -x 192.168.57.30
